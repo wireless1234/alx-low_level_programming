d2cc9fba17ce4d1a20e99c2cd7bbfd43ca0622f8
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-07 13:31:12 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *_strlen_recursion - get string length
+ *@s: the string
+ *Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
