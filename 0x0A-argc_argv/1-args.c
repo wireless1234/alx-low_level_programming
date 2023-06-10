7093e4aeb7fb79ca4f0c03007f70d4572d46bebb
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-09 18:21:00 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - print argc
+ *@argc: argument
+ *@argv: argument
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argc);
+	return (0);
+}
