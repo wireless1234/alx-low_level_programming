bc74596cac1f299fd3334b3678bc768b625506d6
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-09 18:06:04 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ *main - show fnction name
+ *@argc: argument
+ *@argv: argument
+ *Return: 0
+ */ 
+int main(int __attribute__ ((unused)) argc, char **argv)
+{
+	printf("%s\n",argv[0]);
+	exit(EXIT_SUCCESS);
+}
