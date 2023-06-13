5f65408053fe58455a60f883028910b500d33957
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-13 12:54:53 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,27 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conStr;
-	int length = strlen(s1) + strlen(s2) + 1;
+	int i;
 
-
+	if (s1 || s2 == NULL)
+	{
+		return (NULL);
+	}
+	conStr = (char *)malloc((strlen(s1) + strlen(s2)) * sizeof(char) + 1);
+	if (conStr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= (int)(strlen(s1) + strlen(s2)); i++)
+	{
+		if (i < (int)strlen(s1))
+		{
+			conStr[i] = s1[i];
+		}
+		else
+		{
+			conStr[i] = s2[i - (int)strlen(s1)];
+		}
+	}
+	return (conStr);
 }
