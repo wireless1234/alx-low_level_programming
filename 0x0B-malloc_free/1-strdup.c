9f3fa2f22c3dc4dc9cd30789ab8eed1430c2b870
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-13 09:37:47 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ *_strdup - create memory that contain string
+ *@str: pointer
+ *Return: character pointer
+ */
+char *_strdup(char *str)
+{
+	char *myStr;
+	int length = strlen(str);
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		myStr = (char *)malloc(length * sizeof(char) + 1);
+		if (myStr == NULL)
+		{
+			return (NULL);
+		}
+	}
+	while (i <= length)
+	{
+		myStr[i] = str[i];
+		i++;
+	}
+	return (myStr);
+}
