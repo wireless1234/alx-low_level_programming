8c3e6bb626c168839d8d6c87f8f7482cfc935061
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-14 04:40:44 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,6 +10,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **newgrid;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 	{
@@ -20,15 +21,15 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		newgrid[i] = (int *)malloc(width * sizeof(int));
 		if (newgrid[i] == NULL)
 			return (NULL);
 	}
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (int j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 		{
 			newgrid[i][j] = 0;
 		}
