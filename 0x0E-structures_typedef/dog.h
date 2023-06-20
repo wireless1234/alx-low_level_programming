8fc83d9a83ec2fd094ec2c27aa5b3c896ea6e8e5
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-20 06:55:11 -0700

    struct dog description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-struct dog{
+/**
+ *struct dog - dog details
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
