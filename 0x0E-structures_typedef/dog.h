b3ca1a04a0e81e2c40cf01566cb0950f89ddd099
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-20 06:50:38 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
