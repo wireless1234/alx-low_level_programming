0d9d1a9e636df375d8f9439ec515ad95a997e571
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-21 12:26:16 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int i;
 	int length = (int)size;
 
+	if (size == NULL || array == NULL || action == NULL)
+		return;
 	for (i = 0; i < length; i++)
 	{
 		action(array[i]);
