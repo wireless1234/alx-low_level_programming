053a2bd92b9f48c417e2aa472dc3dc66834c8615
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-20 17:56:10 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,5 +9,5 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name:%s\nAge:%f\nOwner:%s\n", d->name, d->age, d->owner)
+	printf("Name:%s\nAge:%f\nOwner:%s\n", d->name, d->age, d->owner);
 }
