98335dd7e391ae5622d88883fecd6d0ac91dc373
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-06-23 17:27:44 -0700

    initial commit for task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_numbers - print all numbers
+ *@separator: agument
+ *@n: argument
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list mylist;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+	if (separator == NULL)
+		return;
+	va_start(mylist, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(mylist, int));
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
