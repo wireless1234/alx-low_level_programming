b7dffcefb304d2da571fb5dfb174b7978055dce6
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-07-06 18:30:37 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: pointer to pointer
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+
+}
