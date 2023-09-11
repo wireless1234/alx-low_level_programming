5cf6c82703b6791eaf519924aa05828af2aa2096
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-11 11:42:33 +0100

    added check for malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	int i = 0;
 
 	hash_table_t *table = malloc(sizeof(*hash_table_t));
+	if (table == NULL)
+		return (NULL);
 	table->size = size;
 	table->array = malloc(size * sizeof(*hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
 
 	while(i < size)
 	{
