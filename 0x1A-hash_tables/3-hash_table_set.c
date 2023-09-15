d9820de96c62fa7f783a6d252c4ffa1741e195b9
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-15 15:45:39 +0100

    task 3 - 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current_node->key, key))
 		{
 			free(current_node->value);
-			current_node->value = value;
+			current_node->value = strdup(value);
 			return (1);
 		}
 		current_node = current_node->next;
