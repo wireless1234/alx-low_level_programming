cb3aeb162d1605f9882e5e91e7f6fe80cde927db
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-15 15:40:46 +0100

    task 3 - 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,17 +12,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index_value;
 	unsigned long int size = ht->size;
 	hash_node_t *new = NULL;
+	hash_node_t *current_node;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	index_value = key_index((const unsigned char *)key, size);
+	current_node = ht->array[index_value];
+	while (current_node == 0)
+	{
+		if (strcmp(current_node->key, key))
+		{
+			free(current_node->value);
+			current_node->value = value;
+			return (1);
+		}
+		current_node = current_node->next;
+	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = ht->array[index_value];
-	ht->array[index_value] = new;
+	new->next = current_node;
+	current_node = new;
 	return (1);
 }
 
