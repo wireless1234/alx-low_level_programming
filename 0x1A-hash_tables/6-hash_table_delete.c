7fba0bbe607aa333f36ac24f2f6a0a3910e36c00
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-11-04 15:57:48 +0100

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,25 +9,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *element, *temp;
+	unsigned long int i;
+	hash_node_t *element, *temp;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        element = ht->array[i];
-        while (element != NULL)
-        {
-            temp = element->next;
-            free(element->key);
-            free(element->value);
-            free(element);
-            element = temp;
-        }
-    }
+	for (i = 0; i < ht->size; i++)
+	{
+		element = ht->array[i];
+		while (element != NULL)
+		{
+			temp = element->next;
+			free(element->key);
+			free(element->value);
+			free(element);
+			element = temp;
+		}
+	}
 
-    free(ht->array);
-    free(ht);
+	free(ht->array);
+	free(ht);
 }
