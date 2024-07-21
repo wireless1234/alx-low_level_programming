805caf91a948ede7d3a073d25349b3207f2ee8af
isa sulaiman isa <isasulaimanisa@gmail.com>
2024-07-21 04:46:09 +0100

    fixed task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -23,9 +23,9 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = %d\n", i, array[i]);
 		if (array[i] == value)
 		{
-			break;
+			return (i);
 		}
 		i++;
 	}
-	return (i);
+	return (-1);
 }
